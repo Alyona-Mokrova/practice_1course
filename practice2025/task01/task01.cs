d5178ab2df78ff0e8f6c9a05a94d95223e619171
Alyona-Mokrova <mokrovaa9u@gmail.com>
2025-06-27 20:10:39 +0600

    feat: add task01

---
+++ practice2025/task01/task01.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public static class StringExtensions
+{
+    public static bool IsPalindrome(this string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+            return false;
+
+        string cleanedInput = "";
+        foreach (char c in input.ToLower())
+        {
+            if (!char.IsWhiteSpace(c) && !char.IsPunctuation(c))
+                cleanedInput += c;
+        }
+        string reversedInput = "";
+        for (int i = cleanedInput.Length - 1; i >= 0; i--)
+        {
+            reversedInput += cleanedInput[i];
+        }
+
+        return cleanedInput == reversedInput;
+    }
+}
