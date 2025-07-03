50e80b67b1d3c8a362ce17619a11b9e033d2e196
Alyona-Mokrova <mokrovaa9u@gmail.com>
2025-07-03 17:23:28 +0600

    feat: add task05

---
+++ practice2025/task05tests/task05tests.cs
@@ -0,0 +1,64 @@
+﻿using Xunit;
+using task05;
+
+namespace task05tests
+{
+    public class TestClass
+    {
+        public int PublicField;
+        private string _privateField;
+        public int Property { get; set; }
+
+        public void Method() { }
+        public int MethodWithParams(int x1, string x2) => 0;
+    }
+
+    [Serializable]
+    public class AttributedClass { }
+
+    public class ClassAnalyzerTests
+    {
+        [Fact]
+        public void GetPublicMethods_ReturnsCorrectMethods()
+        {
+            var analyzer = new ClassAnalyzer(typeof(TestClass));
+            var methods = analyzer.GetPublicMethods();
+
+            Assert.Contains("Method", methods);
+        }
+
+        [Fact]
+        public void GetAllFields_IncludesPrivateFields()
+        {
+            var analyzer = new ClassAnalyzer(typeof(TestClass));
+            var fields = analyzer.GetAllFields();
+
+            Assert.Contains("_privateField", fields);
+        }
+
+        [Fact]
+        public void GetProperties_Returns_PropertyNames()
+        {
+            var analyzer = new ClassAnalyzer(typeof(TestClass));
+            var properties = analyzer.GetProperties();
+
+            Assert.Contains("Property", properties);
+        }
+
+        [Fact]
+        public void HasAttribute_Returns_TrueIfAttributeExists()
+        {
+            var analyzer = new ClassAnalyzer(typeof(AttributedClass));
+
+            Assert.True(analyzer.HasAttribute<SerializableAttribute>());
+        }
+
+        [Fact]
+        public void HasAttribute_Returns_FalseIfAttributeNotPresent()
+        {
+            var analyzer = new ClassAnalyzer(typeof(TestClass));
+
+            Assert.False(analyzer.HasAttribute<SerializableAttribute>());
+        }
+    }
+}
