51f260399c052dfd206744f9540e73c945a2ab86
Alyona-Mokrova <mokrovaa9u@gmail.com>
2025-07-06 18:59:21 +0600

    feat: add task08

---
+++ practice2025/FileSystemCommands/Class1.cs
@@ -0,0 +1,45 @@
+﻿public class DirectorySizeCommand : ICommand
+{
+    private readonly string pathDirectory;
+    public long DirectorySize { get; private set; }
+
+    public DirectorySizeCommand(string pathDirectory)
+    {
+        this.pathDirectory = pathDirectory;
+    }
+
+    public void Execute()
+    {
+        DirectorySize = GetDirectorySize(pathDirectory);
+    }
+
+    private long GetDirectorySize(string pathDirectory)
+    {
+        return Directory
+            .EnumerateFiles(pathDirectory, "*", SearchOption.AllDirectories)
+            .Sum(file => new FileInfo(file).Length);
+    }
+
+}
+
+public class FindFilesCommand : ICommand
+{
+    private readonly string pathDirectory;
+    private readonly string searchMask;
+    public string[] FilesWithMask { get; private set; } = Array.Empty<string>();
+
+    public FindFilesCommand(string pathDirectory, string searchMask)
+    {
+        this.pathDirectory = pathDirectory; 
+        this.searchMask = searchMask;
+    }
+
+    public void Execute()
+    {
+        if (!Directory.Exists(pathDirectory))
+            throw new DirectoryNotFoundException(pathDirectory);
+
+        FilesWithMask = Directory.GetFiles(pathDirectory, searchMask, SearchOption.TopDirectoryOnly);
+
+    }
+}
